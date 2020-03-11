4631ecf948d08bafd1fe64754dd9a2d9b7fbd3ca
ReksioCroft <octavian-florin.staicu@my.fmi.unibuc.ro>
2020-03-11 16:38:25 +0200

    implementare * TODO: de termiant try & catch

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <functional>
+
 const int INF = (1<<29);
 using namespace std;
 
+int mini( int a, int b ) {
+    return ( a < b ) ? a : b;
+}
+
 class Graf_ponderat {
 private:
-    vector< pair< int, int> > v[100];                             ///presupun ca am maxim 100 noduri intr-un graf
+    static const int maxNod = 100;
+    vector< pair< int, int> > v[maxNod];                             ///presupun ca am maxim maxNod noduri intr-un graf
     void printare_bfs( int drum[], int nod1, int nod2 );         ///le retin ca lista de adiacenta; .first va contine nodul in care mergem, si .second ponderea muchiei
     void dfs_conex( bool nod[], int poz );
 public:
@@ -31,6 +38,8 @@ public:
     void pondere_roy_floyd();
 
     bool conex();
+
+    Graf_ponderat &operator *(const Graf_ponderat & g2 );
 };
 
 Graf_ponderat::Graf_ponderat() {///constructor initializare
@@ -47,19 +56,19 @@ Graf_ponderat::Graf_ponderat() {///constructor initializare
 
 Graf_ponderat::Graf_ponderat( const Graf_ponderat &g ) {///constructor copiere
     int i, j;
-    for ( i = 0; i < 100; i++ )
+    for ( i = 0; i < maxNod; i++ )
         for ( j = 0; j < g.get_v_size( i ); j++ )
             v[i].push_back( g.get_nod( i, j ) );
 }
 
 Graf_ponderat::~Graf_ponderat() {///destructor
-    for( int i = 0; i < 100; i++ )
+    for( int i = 0; i < maxNod; i++ )
         v[i].clear();
 }
 
 ostream &operator <<( ostream& afisare, const Graf_ponderat &g ) {
     int i, j;
-    for ( i = 0; i < 100; i++ )
+    for ( i = 0; i < Graf_ponderat::maxNod; i++ )
         for ( j = 0; j < g.v[i].size(); j++ )
             if ( i < g.v[i][j].first ) ///fiind graf neorientat, voi afisa o singura data muchiile
                 afisare<<"Exista drum de la nodul " << i << " la nodul " << g.v[i][j].first <<" cu ponderea " << g.v[i][j].second << "\n";
@@ -67,9 +76,9 @@ ostream &operator <<( ostream& afisare, const Graf_ponderat &g ) {
 }
 
 void Graf_ponderat::bfs_bellman_ford(int nod, int nod2) {
-    int nod1 = nod, i, drum[100], cost[100];
+    int nod1 = nod, i, drum[maxNod], cost[maxNod];
     queue < int > bellman;
-    for ( i = 0; i < 100; i++ ) {
+    for ( i = 0; i < maxNod; i++ ) {
         drum[i] = cost[i] = INF;
     }
     cost[nod1] = 0;
@@ -103,7 +112,7 @@ void Graf_ponderat::printare_bfs( int drum[], int nod1, int nod2 ) {
 void Graf_ponderat::pondere_roy_floyd() {
     int i, maxi, j, k;
     maxi = 0;
-    for ( i = 0; i < 100; i++ )
+    for ( i = 0; i < maxNod; i++ )
         if ( v[i].empty() == 0 )
             maxi = i;
     int matrix[maxi+1][maxi+1];
@@ -145,22 +154,65 @@ void Graf_ponderat::dfs_conex(bool *nod, int poz) {
 }
 
 bool Graf_ponderat::conex() {
-    bool vizitat[100] = {false};
+    bool vizitat[maxNod] = {false};
     int i = 0;
     while ( v[i].empty() )
         i++;
     dfs_conex( vizitat, i );
-    for ( i = 0; i < 100; i++ )
+    for ( i = 0; i < maxNod; i++ )
         if ( v[i].empty() == 0 && vizitat[i] == false )
-            return  false;
+            return false;
     return true;
 }
 
+Graf_ponderat& Graf_ponderat::operator *(const Graf_ponderat &g2) {
+    int i, j, k;
+    bool ok = true, gasit;
+    try {
+        for ( i = 0; i < maxNod; i++ )
+            if ( this->v[i].empty() != g2.v[i].empty() )
+               __throw_invalid_argument( "Grafurile nu au aceeasi multime de muchii. ");
+    }
+    catch ( const invalid_argument &e ){
+        cout << e.what();
+        ok = false;
+    }
+
+    if ( ok == true ) {
+        for ( i = 0; i < maxNod; i++ ) {
+            for ( j = 0; j < v[i].size(); j++ ) {
+                gasit = false;
+                for ( k = 0; k < g2.v[i].size() && gasit == false; k++ ) {
+                    if ( v[i][j].first == g2.v[i][j].first ) {
+                        gasit = true;
+                        v[i][j].second = mini( v[i][j].second, g2.v[i][j].second );
+                    }
+                }
+                if ( gasit == false ) {
+                    v[i].erase( v[i].begin() + j, v[i].begin() + j + 1 );
+                }
+            }
+        }
+        return *this;
+    }
+    else
+        __throw_bad_function_call();
+}
+
 int main() {
-    Graf_ponderat g;
-    cout << g;
-    g.bfs_bellman_ford( 1, 2 );
-    g.pondere_roy_floyd();
-    cout <<  ( ( g.conex() == true ) ? "Graful este conex\n" : "Graful nu este conex\n" );
+    try {
+        Graf_ponderat g;
+        cout << g;
+        g.bfs_bellman_ford(1, 2);
+        g.pondere_roy_floyd();
+        cout << ((g.conex() == true) ? "Graful este conex\n" : "Graful nu este conex\n");
+        Graf_ponderat g2;
+        cout << g * g2;
+        Graf_ponderat g3;       ///TODO
+        cout << g * g2 * g;
+    }
+    catch (const bad_function_call &e){
+        cout << "\"Alta-ntrebare?!\"\n";
+    }
     return 0;
 }
